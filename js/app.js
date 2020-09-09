5bd189ba8586f61d920ff8e787bfc506eaa51560
Doktor-Doom <andre.o.martin@gmail.com>
2020-09-09 08:11:52 -0700

    the hunt for missing semicolons, and converted constructor to class ES2015 which added the everything HORN to the same scope and somehow not breaking anything, its mysterious

--- js/app.js
+++ js/app.js
@@ -9,46 +9,48 @@ $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' })
   })
   .then(() => Horn.dropDown());
 
-function Horn(object) {
-  this.imgUrl = object.image_url
-  this.title = object.title
-  this.description = object.description
-  this.keyword = object.keyword
-  this.horns = object.horns
-  hornArray.push(this)
+class Horn {
+  constructor(object) {
+    this.imgUrl = object.image_url;
+    this.title = object.title;
+    this.description = object.description;
+    this.keyword = object.keyword;
+    this.horns = object.horns;
+    hornArray.push(this);
+  }
+  render() {
+    const template = $('#photo-template').html();
+    const $newSection = $(`<section id="${this.keyword}">${template}</section>`);
+    $newSection.find('h2').text(this.title);
+    $newSection.find('p').text(`${this.description}. Number of horns ${this.horns}`);
+    $newSection.find('img').attr('src', this.imgUrl);
+    $newSection.find('img').attr('title', this.title);
+
+    $('main').append($newSection);
+  }
+  static dropDown() {
+    let tempArray = [];
+    hornArray.forEach(value => {
+      if (!tempArray.includes(value.keyword)) {
+        tempArray.push(value.keyword);
+      }
+    });
+    tempArray.forEach(value => {
+      const $newOptionTag = $(`<option value="${value}">${value}</option>`);
+      $('select').append($newOptionTag);
+    });
+  }
 }
 
-Horn.prototype.render = function () {
-  const template = $('#photo-template').html();
-  const $newSection = $(`<section id="${this.keyword}">${template}</section>`);
-  $newSection.find('h2').text(this.title)
-  $newSection.find('p').text(` ${this.description}. Number of horns ${this.horns}`);
-  $newSection.find('img').attr('src', this.imgUrl);
-  $newSection.find('img').attr('title', this.title)
 
-  $('main').append($newSection);
-}
-
-Horn.dropDown = () => {
-  let tempArray = [];
-  hornArray.forEach(value => {
-    if (!tempArray.includes(value.keyword)) {
-      tempArray.push(value.keyword);
-    }
-  })
-  tempArray.forEach(value => {
-    const $newOptionTag = $(`<option value="${value}">${value}</option>`);
-    $('select').append($newOptionTag);
-  })
-}
 
 $('select').on('change', handler);
 
 function handler(event) {
   $('section').hide();
   hornArray.forEach((object) => {
-  if(event.target.value === object.keyword) {
-$(`section[id = ${object.keyword}]`).show();
+    if(event.target.value === object.keyword) {
+      $(`section[id = ${object.keyword}]`).show();
   }
 });
 }
